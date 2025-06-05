f60f1ebc1ad86f113eb842742cb22b44233d3d13
1seyoung <akdlrnal0705@gmail.com>
2025-06-06 01:35:43 +0900

    init : project setting

---
+++ src/main/java/com/archiservice/ArchiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.archiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArchiServiceApplication.class, args);
+  }
+
+}
