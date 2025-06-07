d4b1db27aca118269eafd2eb508972f83986e96e
lbk00 <lbk6661@gmail.com>
2025-06-07 14:40:20 +0900

    feat : user 엔티티, 레포지토리 추가

---
+++ src/main/java/com/archiservice/user/domain/User.java
@@ -0,0 +1,50 @@
+package com.archiservice.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    @Column(name = "user_id")
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "age_code")
+    private String ageCode;
+
+    @Column(name = "tag_code")
+    private Long tagCode;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/archiservice/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.archiservice.user.repository;
+
+import com.archiservice.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
