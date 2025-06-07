5d729b437eafbd7097a4af72312e5ad61d855dd9
GihwanPark <pgh5121@naver.com>
2025-06-07 23:39:44 +0900

    refactor: @EnableJpaAuditing 추가

--- src/main/java/com/archiservice/ArchiServiceApplication.java
+++ src/main/java/com/archiservice/ArchiServiceApplication.java
@@ -2,10 +2,11 @@ package com.archiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class}) // 테스트할때만 붙임
 @EnableJpaAuditing
-@SpringBootApplication
 public class ArchiServiceApplication {
 
   public static void main(String[] args) {
