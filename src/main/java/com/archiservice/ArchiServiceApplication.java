d704d818651ae3e9687f858a4b14126d1173edca
Jang-GO <jang8195@naver.com>
2025-06-08 17:05:29 +0900

    chore: JpaAuditing 활성화 및 주석제거

--- src/main/java/com/archiservice/ArchiServiceApplication.java
+++ src/main/java/com/archiservice/ArchiServiceApplication.java
@@ -2,9 +2,10 @@ package com.archiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class}) // 테스트할때만 붙임
+@SpringBootApplication
+@EnableJpaAuditing
 public class ArchiServiceApplication {
 
   public static void main(String[] args) {
