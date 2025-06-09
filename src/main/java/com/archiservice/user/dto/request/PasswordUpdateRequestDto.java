4718cc4d8cd7b98faf7fabedf28f0407bf23e6de
Gihwan-Park <pgh5121@naver.com>
2025-06-09 16:17:42 +0900

    feat : 프로필 조회, 비밀번호 변경, 성향 조회

--- src/main/java/com/archiservice/user/dto/request/PasswordUpdateRequestDto.java
+++ src/main/java/com/archiservice/user/dto/request/PasswordUpdateRequestDto.java
@@ -1,8 +1,10 @@
 package com.archiservice.user.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PasswordUpdateRequestDto {
     private String password;
     private String newPassword;
