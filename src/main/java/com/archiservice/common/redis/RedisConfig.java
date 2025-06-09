e39ddda6277a2628602e3f89362d61b56660f264
lbk00 <lbk6661@gmail.com>
2025-06-09 14:34:40 +0900

    feat : @EnableCaching 추가

--- src/main/java/com/archiservice/common/redis/RedisConfig.java
+++ src/main/java/com/archiservice/common/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.archiservice.common.redis;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
