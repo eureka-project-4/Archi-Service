27765e917e40381281156f7cdbdd7ed12f0b49ae
Jang-GO <jang8195@naver.com>
2025-06-09 14:55:01 +0900

    feat: 부가서비스 조회 API 구현

---
+++ src/main/java/com/archiservice/product/vas/controller/VASController.java
@@ -0,0 +1,29 @@
+package com.archiservice.product.vas.controller;
+
+import com.archiservice.common.response.ApiResponse;
+import com.archiservice.product.vas.dto.response.VASDetailResponseDto;
+import com.archiservice.product.vas.dto.response.VASResponseDto;
+import com.archiservice.product.vas.service.VASService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vas")
+@RequiredArgsConstructor
+public class VASController {
+
+    private final VASService vasService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<VASResponseDto>>> getAllVASs() {
+        return ResponseEntity.ok(ApiResponse.success(vasService.getAllVASs()));
+    }
+
+    @GetMapping("/{serviceId}")
+    public ResponseEntity<ApiResponse<VASDetailResponseDto>> getVASDetail(@PathVariable("serviceId") Long serviceId) {
+        return ResponseEntity.ok(ApiResponse.success(vasService.getVASDetail(serviceId)));
+    }
+}
