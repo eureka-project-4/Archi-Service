117fb754865a5b9bdcd9222149bac1dbb0566c48
Jang-GO <jang8195@naver.com>
2025-06-10 15:01:14 +0900

    feat: @Valid 검증 실패시 적절한 예외메시지 반환

--- src/main/java/com/archiservice/exception/ErrorCode.java
+++ src/main/java/com/archiservice/exception/ErrorCode.java
@@ -18,6 +18,7 @@ public enum ErrorCode {
   PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT_NOT_FOUND", "상품을 찾을 수 없습니다."),
   REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW_NOT_FOUND", "리뷰를 찾을 수 없습니다."),
   NOT_MY_REVIEW(HttpStatus.FORBIDDEN, "NOT_MY_REVIEW", "본인의 리뷰만 수정 또는 삭제할 수 있습니다."),
+  INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID_INPUT_VALUE", "올바르지 않은 입력값입니다."),
 
   ALREADY_REVIEWED(HttpStatus.BAD_REQUEST, "ALREADY_REVIEWED", "이미 해당 요금제에 대한 리뷰를 작성하셨습니다.");
 
