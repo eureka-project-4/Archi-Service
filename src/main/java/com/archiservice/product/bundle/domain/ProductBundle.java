7545ba092dd3922571c4b318a23b6784a771479e
Gihwan-Park <pgh5121@naver.com>
2025-06-11 10:24:01 +0900

    feat : 결제 영수증 조회 & 예약 취소

---
+++ src/main/java/com/archiservice/product/bundle/domain/ProductBundle.java
@@ -0,0 +1,46 @@
+package com.archiservice.product.bundle.domain;
+
+import com.archiservice.product.coupon.domain.Coupon;
+import com.archiservice.product.plan.domain.Plan;
+import com.archiservice.product.vas.domain.Vas;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "product_bundles")
+public class ProductBundle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_bundle_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_id")
+    private Vas vas;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    private long likeCount;
+    private long disLikeCount;
+    private long tagCode;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
