0fcfc4e7dca22099197621473f2916b5f3dc1fdf
lbk00 <lbk6661@gmail.com>
2025-06-12 10:37:09 +0900

    feat : 개별 요금제 추천 api 추가

--- src/main/java/com/archiservice/recommend/controller/RecommendController.java
+++ src/main/java/com/archiservice/recommend/controller/RecommendController.java
@@ -3,7 +3,10 @@ package com.archiservice.recommend.controller;
 
 import com.archiservice.common.response.ApiResponse;
 import com.archiservice.common.security.CustomUser;
+import com.archiservice.recommend.dto.response.RecommendCouponResponseDto;
+import com.archiservice.recommend.dto.response.RecommendPlanResponseDto;
 import com.archiservice.recommend.dto.response.RecommendResponseDto;
+import com.archiservice.recommend.dto.response.RecommendVasResponseDto;
 import com.archiservice.recommend.service.RecommendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,21 @@ public class RecommendController {
     @GetMapping()
     public ResponseEntity<ApiResponse<RecommendResponseDto>> recommend(@AuthenticationPrincipal CustomUser customUser){
         return ResponseEntity.ok(ApiResponse.success("조합 추천 성공", recommendService.recommend(customUser)));
+    }
+
+    @GetMapping("/plan")
+    public ResponseEntity<ApiResponse<RecommendPlanResponseDto>> recommendPlan(@AuthenticationPrincipal CustomUser customUser){
+        return ResponseEntity.ok(ApiResponse.success("요금제 추천 성공", recommendService.recommendPlan(customUser)));
+    }
+
+    @GetMapping("/vas")
+    public ResponseEntity<ApiResponse<RecommendVasResponseDto>> recommendVas(@AuthenticationPrincipal CustomUser customUser){
+        return ResponseEntity.ok(ApiResponse.success("부가서비스 추천 성공", recommendService.recommendVas(customUser)));
+    }
+
+    @GetMapping("/coupon")
+    public ResponseEntity<ApiResponse<RecommendCouponResponseDto>> recommendCoupon(@AuthenticationPrincipal CustomUser customUser){
+        return ResponseEntity.ok(ApiResponse.success("쿠폰 추천 성공", recommendService.recommendCoupon(customUser)));
     }
 
 }
