bc4b41ddcc72cac22d25737a612228448da47554
lbk00 <lbk6661@gmail.com>
2025-06-14 16:02:51 +0900

    fix : AuthInfo -> Principal로 수정

--- src/main/java/com/archiservice/chatbot/controller/WebSocketController.java
+++ src/main/java/com/archiservice/chatbot/controller/WebSocketController.java
@@ -3,15 +3,9 @@ package com.archiservice.chatbot.controller;
 import com.archiservice.chatbot.domain.AuthInfo;
 import com.archiservice.chatbot.dto.request.ChatMessageRequestDto;
 import com.archiservice.chatbot.service.ChatService;
-import com.archiservice.common.security.CustomUser;
-import com.archiservice.user.domain.User;
-import com.archiservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -21,7 +15,6 @@ import java.security.Principal;
 public class WebSocketController {
 
     private final ChatService chatService;
-
 
     @MessageMapping("/chat/sendMessage")
     public void sendMessage(
